887ec21f20a96f30def66bcc60e60e5d7df1f4a9
cree321 <42984414+cree321@users.noreply.github.com>
2023-04-21 02:36:36 -0700

    Init commit

---
+++ script/engine/sceneDelegate.js
@@ -0,0 +1,33 @@
+"use strict";
+var commit = ``;
+var isParsing = 0;
+const cap0ul = new Request("https://raw.githubusercontent.com/cree321/ned/master/assets/cap0/escena.json");
+
+parseMap(cap0ul);
+
+onmessage = (message) => {
+  var timeout = setTimeout(() => {
+    clearInterval(checker);
+    console.log("sD: Timeout");
+  }, 10100);
+  var checker = setInterval(() => {
+    if(!isParsing) {
+      postMessage(commit);
+      clearTimeout(timeout);
+      clearInterval(checker);
+    }
+  }, 1000);
+}
+
+function parseMap(request) {
+  commit = ``;
+  fetch(request)
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data);
+      commit += `<link rel="stylesheet" href="${data.link}"></link>`;
+      data.hall.forEach((value) => {
+        commit += `<div class="geo ${value.c}" style="transform: translate3d(${value.t[0]}px,${value.t[1]}px,${value.t[2]}px) scale3d(${value.s[0]},${value.s[1]},${value.s[2]}) rotateX(${value.r[0]}deg) rotateY(${value.r[1]}deg);"></div>`;
+      });
+    }).catch(console.error);
+}
