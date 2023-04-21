54713e579a0c228901a2ff023a0a13e2d6f89231
cree321 <42984414+cree321@users.noreply.github.com>
2023-04-21 03:19:03 -0700

    Update sw.js

--- script/sw.js
+++ script/sw.js
@@ -10,12 +10,12 @@ const APP_CONTENT = [
 const APP_CACHE = APP_SHELL.concat(APP_CONTENT);
 
 self.addEventListener("install", (e) => {
-  console.log("[Service Worker] Install");
+  console.debug("[Service Worker] Install");
 
   e.waitUntil(
     (async () => {
       const CACHE = await caches.open(CACHE_NAME);
-      console.log("[Service Worker] Caching all: app shell and content");
+      console.debug("[Service Worker] Caching all: app shell and content");
       await CACHE.addAll(APP_CACHE);
     })()
   );
@@ -25,13 +25,13 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
       const CACHED_RESOURCE = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      console.debug(`[Service Worker] Fetching resource: ${e.request.url}`);
       if (CACHED_RESOURCE) {
         return CACHED_RESOURCE;
       }
       const NEW_RESOURCE = await fetch(e.request);
       const CACHE = await caches.open(CACHE_NAME);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      console.debug(`[Service Worker] Caching new resource: ${e.request.url}`);
       CACHE.put(e.request, NEW_RESOURCE.clone());
       return NEW_RESOURCE;
     })()
