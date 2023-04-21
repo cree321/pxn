633f422f01ba8cd7f233b8094de103d903476e74
cree321 <42984414+cree321@users.noreply.github.com>
2023-04-21 03:18:27 -0700

    Update sceneDelegate.js

--- script/engine/sceneDelegate.js
+++ script/engine/sceneDelegate.js
@@ -8,7 +8,7 @@ parseMap(cap0ul);
 onmessage = (message) => {
   var timeout = setTimeout(() => {
     clearInterval(checker);
-    console.log("sD: Timeout");
+    console.debug("sD: Timeout");
   }, 10100);
   var checker = setInterval(() => {
     if(!isParsing) {
@@ -24,7 +24,7 @@ function parseMap(request) {
   fetch(request)
     .then(response => response.json())
     .then(data => {
-      //console.log(data);
+      //console.debug(data);
       commit += `<style>.tempgrad {background-image: linear-gradient(#f00, #0f0);}</style>`;//`<link rel="stylesheet" href="${data.link}"></link>`;
       data.hall.forEach((value) => {
         commit += `<div class="geo ${value.c}" style="transform: translate3d(${value.t[0]}px,${value.t[1]}px,${value.t[2]}px) scale3d(${value.s[0]},${value.s[1]},${value.s[2]}) rotateX(${value.r[0]}deg) rotateY(${value.r[1]}deg);"></div>`;
