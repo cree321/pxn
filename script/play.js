0fe160048a15c97fbd452e2b1235ad348f04f5a9
cree321 <42984414+cree321@users.noreply.github.com>
2026-06-15 01:59:28 -0700

    Update play.js

--- script/play.js
+++ script/play.js
@@ -43,6 +43,7 @@ window.onload = (event) => {
     // const control_destroy = document.getElementById("controlUI_destroy");
 
     window.addEventListener("pointermove", (e) => {
+      if(e.target === control_move_center) return;
       // switch(e.target.id)
       // {
       //   case "controlUI_look":
