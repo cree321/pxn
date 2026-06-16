3acfef161e6a014c42e953b1e16261f68836b660
cree321 <42984414+cree321@users.noreply.github.com>
2026-06-15 23:45:27 -0700

    fix joystick move direction

--- script/engine/physicsDelegate.js
+++ script/engine/physicsDelegate.js
@@ -11,7 +11,7 @@ var acceleration = [0,0,0];// which direction is the viewer intending to move
 var velocity = [0,0,0];// which direction/magnitude is the viewer moving
 var rotV = [0,0];// rotation velocity
 var lookSensitivity = [1,1];
-var moveSensitivity = [1, 1];
+var moveSensitivity = [10, 10];
 var maxFall = 0;// distance to floor
 // POST: Input Map???
 
@@ -21,7 +21,7 @@ onmessage = (message) => {
   //console.debug(e);
   switch(e.type) {
     case 3:
-      velocity[2] = -e.movementY * moveSensitivity[1];
+      velocity[2] = e.movementY * moveSensitivity[1];
       velocity[0] = -e.movementX * moveSensitivity[0];
       break;
     case 2:
